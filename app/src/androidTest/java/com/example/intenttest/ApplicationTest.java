b566a4f1043feef0787077a5b2937b006c280222
352328690 <352328690@qq.com>
2016-03-02 18:20:45 +0800

    使用Intent传递对象两种方法 1,Serializable  序列化方式较简单但是效率低 2,Parcelable  方法复杂一些但是效率高

--- app/src/androidTest/java/com/example/intenttest/ApplicationTest.java
+++ app/src/androidTest/java/com/example/intenttest/ApplicationTest.java
@@ -10,4 +10,20 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    public void test() throws Exception{
+        final int a = 1;
+        final int b = 2;
+        assertEquals(a, b);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 }
